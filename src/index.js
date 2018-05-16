b67add064e1c1c92b307a40b7002d7d01babd1e8
Tulio <tuliomac@gmail.com>
2018-05-16 14:10:04 -0300

    fazendo o dropdopwn do menu principal e organizando conforme modelo de arquitetura

--- src/index.js
+++ src/index.js
@@ -6,3 +6,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import '../assets/scss/main.scss';
 
+$('.dropdown-toggle').hover(function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeIn(100);
+}, function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeOut(100);
+});
